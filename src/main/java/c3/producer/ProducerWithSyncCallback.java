c58b4df67c06239ae084ab85206cffbfed4f6e73
seokjin0414 <sars21@hanmail.net>
2023-05-12 14:29:28 +0900

    SyncCallback --OK

--- src/main/java/c3/producer/ProducerWithSyncCallback.java
+++ src/main/java/c3/producer/ProducerWithSyncCallback.java
@@ -3,10 +3,15 @@ package c3.producer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Properties;
 
-public class ProducerExactPartition {
+public class ProducerWithSyncCallback {
+    private final static Logger logger = LoggerFactory.getLogger(ProducerWithSyncCallback.class);
     private final static String TOPIC_NAME = "hello.kafka";
     private final static String BOOTSTRAP_SERVERS = "my-kafka:9092";
 
@@ -16,14 +21,19 @@ public class ProducerExactPartition {
         configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
         configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        //configs.put(ProducerConfig.ACKS_CONFIG, "0");
 
         KafkaProducer<String, String> producer = new KafkaProducer<>(configs);
 
-        int partitionNo = 0;
-        ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC_NAME, partitionNo, "Pangyo0000", "Pangyo000");
-        producer.send(record);
-
-        producer.flush();
-        producer.close();
+        ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC_NAME, "BNB", "Pangyo");
+        try {
+            RecordMetadata metadata = producer.send(record).get();
+            logger.info(metadata.toString());
+        } catch (Exception e) {
+            logger.error(e.getMessage(),e);
+        } finally {
+            producer.flush();
+            producer.close();
+        }
     }
 }
--- src/main/java/c3/producer/custom/ProducerWithCustomPartitioner.java
+++ src/main/java/c3/producer/custom/ProducerWithCustomPartitioner.java
@@ -1,4 +1,4 @@
-package c3.producer;
+package c3.producer.custom;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
@@ -6,7 +6,7 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 import java.util.Properties;
 
-public class ProducerExactPartition {
+public class ProducerWithCustomPartitioner {
     private final static String TOPIC_NAME = "hello.kafka";
     private final static String BOOTSTRAP_SERVERS = "my-kafka:9092";
 
@@ -16,13 +16,13 @@ public class ProducerExactPartition {
         configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
         configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        // 커스텀 옵션
+        configs.put(ProducerConfig.PARTITIONER_CLASS_CONFIG, CustomPartitioner.class);
 
         KafkaProducer<String, String> producer = new KafkaProducer<>(configs);
 
-        int partitionNo = 0;
-        ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC_NAME, partitionNo, "Pangyo0000", "Pangyo000");
+        ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC_NAME, "BNB", "testBNB");
         producer.send(record);
-
         producer.flush();
         producer.close();
     }
